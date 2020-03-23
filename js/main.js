6ea93b5e2e207aa7a68e86037802f679dea31d0d
Mariam Khalifa <mariamk@Mariams-MacBook-Pro.local>
2020-03-23 12:11:27 -0400

    progress bar

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+Vue.prototype.$mediaEl = document.querySelector('.media');
 
 var vm = new Vue({
 
@@ -29,6 +30,14 @@ data: {
 },
 
 methods: {
+
+    updateProgress() {
+        let media = document.querySelector('.media'),
+            progress = document.querySelector('#progress');
+
+        progress.value = media.currentTime / media.duration;
+    },
+
     toggleCaptions() {
         let tt = (document.querySelector('.media')).textTracks;
         for(var i=0; i < tt.length; i++) {
@@ -50,8 +59,11 @@ methods: {
         this.media.type = "Movies";
         this.media.name = "Sound of Music";
         this.media.src = "sound_of_music";
-        document.querySelector('.media').load();
         this.transcript.view = false;
+        let media =  document.querySelector('.media');
+        if(media) {
+            document.querySelector('.media').load();
+        }
     },
 
     setSrc2() {
@@ -63,8 +75,11 @@ methods: {
         this.media.type = "TV";
         this.media.name = "Seinfeld";
         this.media.src = "seinfeld";
-        document.querySelector('.media').load();
         this.transcript.view = false;
+        let media =  document.querySelector('.media');
+        if(media) {
+            document.querySelector('.media').load();
+        }
     },
 
     setSrc3() {
@@ -76,8 +91,11 @@ methods: {
         this.media.type = "Songs";
         this.media.name = "Don't Speak - No Doubt";
         this.media.src = "dont_speak";
-        document.querySelector('.media').load();
         this.transcript.view = false;
+        let media =  document.querySelector('.media');
+        if(media) {
+            document.querySelector('.media').load();
+        }
     },
 
     playPauseMedia() {
@@ -93,7 +111,8 @@ methods: {
     },
 
     stopMedia() {
-        document.querySelector('.media').pause();
+        this.$mediaEl.pause();
+        //document.querySelector('.media').pause();
         document.querySelector('.media').currentTime = 0;
         this.media.paused = false;
     },
@@ -109,3 +128,4 @@ methods: {
 }
 
 });
+
